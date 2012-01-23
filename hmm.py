638a445deb028039b9ef97aece6accba87113298
Chi-En Wu <jason2506.mail@gmail.com>
2012-01-24 01:17:15 +0800

    Implement the forward/backward algorithm.

--- hmm.py
+++ hmm.py
@@ -69,3 +69,44 @@ class Model(object):
             return 0
         return self._emit_prob[state][symbol]
 
+    def _forward(self, sequence):
+        sequence_length = len(sequence)
+        if sequence_length == 0:
+            return []
+
+        alpha = [{}]
+        for state in self._states:
+            alpha[0][state] = self.start_prob(state) * self.emit_prob(state, sequence[0])
+
+        for index in xrange(1, sequence_length):
+            alpha.append({})
+            for state_to in self._states:
+                prob = 0
+                for state_from in self._states:
+                    prob += alpha[index - 1][state_from] * \
+                        self.trans_prob(state_from, state_to)
+                alpha[index][state_to] = prob * self.emit_prob(state_to, sequence[index])
+
+        return alpha
+
+    def _backward(self, sequence):
+        sequence_length = len(sequence)
+        if sequence_length == 0:
+            return []
+
+        beta = [{}]
+        for state in self._states:
+            beta[0][state] = 1
+
+        for index in xrange(sequence_length - 1, 0, -1):
+            beta.insert(0, {})
+            for state_from in self._states:
+                prob = 0
+                for state_to in self._states:
+                    prob += beta[1][state_to] * \
+                        self.trans_prob(state_from, state_to) * \
+                        self.emit_prob(state_to, sequence[index])
+                beta[0][state_from] = prob
+
+        return beta
+
